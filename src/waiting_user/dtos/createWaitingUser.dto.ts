e7a62431cc9243c9999205e993f54cff0b68cfe6
eungwang1 <scm323@naver.com>
2023-08-14 17:27:08 +0900

    Feat: 턴 시간제한을 위한 로직 작성

--- src/waiting_user/dtos/createWaitingUser.dto.ts
+++ src/waiting_user/dtos/createWaitingUser.dto.ts
@@ -1,6 +1,10 @@
-import { IsString } from 'class-validator';
+import { IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class CreateWaitingUserInput {
   @IsString()
   socketId: string;
+
+  @IsNumber()
+  @IsOptional()
+  turnTimeLimit?: number;
 }
